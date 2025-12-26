4795343c87374730a5f18fe71cb0594e44b50939
Yamin-Oo <u6632752@au.edu>
2025-12-26 11:06:54 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/display-input-data/',
+  base: '/display-data-deploy/',
 })
